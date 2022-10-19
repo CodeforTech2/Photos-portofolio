e29e06b56a7e3029cd2f08ca4e5d0e4cb474b0c6
CodeforTech2 <stoineaalin2501@gmail.com>
2022-10-19 21:56:16 +0300

    Film section photos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const header = document.getElementById('header');
+// const header = document.getElementById('header');
 
-window.addEventListener('scroll', function() {
-    console.log(window.scrollY);
-    header.style.backgroundSize = 100 - +window.scrollY/29+ '%';
-})
+// window.addEventListener('scroll', function() {
+//     console.log(window.scrollY);
+//     header.style.backgroundSize = 200 - +window.scrollY/12+ '%';
+// })
 
 // Black and white section
 // reimagine pictures phone, nz012 in the middle and another 2 3 4 sideways, all from mountain
