e6d99af570b125da474480a98d2bf19d92735c28
CodeforTech2 <stoineaalin2501@gmail.com>
2022-10-20 21:24:56 +0300

    Adding pngs

--- script.js
+++ script.js
@@ -8,3 +8,5 @@
 // Black and white section
 // reimagine pictures phone, nz012 in the middle and another 2 3 4 sideways, all from mountain
 //empty letters in photoshop, picture with us and a half moon
+// div on black-white section, film type width 100% and edited margins and colors to look like a film roll, all picture inside black and white and moved form right to left non stop
+//--
