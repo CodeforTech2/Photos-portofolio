ff32889b6761097665d09a14f0fad9eaad5b2888
Work Laptop <stoineaalin2501@gmail.com>
2022-10-21 17:39:44 +0300

    update minor changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// const header = document.getElementById('header');
+const header = document.getElementById('header');
 
-// window.addEventListener('scroll', function() {
-//     console.log(window.scrollY);
-//     header.style.backgroundSize = 200 - +window.scrollY/12+ '%';
-// })
+window.addEventListener('scroll', function() {
+    header.style.backgroundSize = 103 + +window.scrollY/10+ '%';
+    // header.style.opacity = 1 - +window.scrollY/800+'';
+})
 
 // Black and white section
 // reimagine pictures phone, nz012 in the middle and another 2 3 4 sideways, all from mountain
