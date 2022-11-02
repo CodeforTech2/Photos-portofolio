62de0793b35004203a1b6f14fa9585482b1951f4
CodeforTech2 <stoineaalin2501@gmail.com>
2022-11-02 21:08:10 +0200

    update gallerie 2

---
+++ Galleries pages/Gallerie_2/gallerie_2.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+
+let lastKnownScrollPosition = 0;
+
+window.addEventListener('scroll', (e)=>{
+    lastKnownScrollPosition = window.scrollY;
+    // console.log(lastKnownScrollPosition);
+    if (lastKnownScrollPosition > 300) {
+        container.style.marginTop = '200px';
+    }
+})
