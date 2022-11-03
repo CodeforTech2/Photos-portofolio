b48fd333f18119631d236f114dd293fdcedfe4ce
CodeforTech2 <stoineaalin2501@gmail.com>
2022-11-03 19:49:12 +0200

    border in gallerie 4 and get login modal functioning

---
+++ Galleries pages/Gallerie_4/script.js
@@ -0,0 +1,18 @@
+//=============================== Login Modal form ===============================
+const modalForm = document.getElementById('form');
+const loginButton = document.getElementById('login');
+const loginCloseButton = document.getElementById('close-login');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(e) {
+    if (e.target == modalForm) {
+        modalForm.style.display = 'none';
+    }
+};
+
+loginButton.addEventListener('click', ()=>{
+    modalForm.style.display = 'block';
+})
+
+loginCloseButton.addEventListener('click', ()=>{
+    modalForm.style.display = 'none';
+})
